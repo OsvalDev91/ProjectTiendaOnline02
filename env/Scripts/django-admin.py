5426d93ae7f127fa157e7449b134717f269573cb
Osvaldo Guzman <2505202015@mail.utec.edu.sv>
2023-06-28 14:30:18 -0600

    Se puso el entorno virtual

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Practicas de josue\Proyectos\ProjectTiendaOnline02\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
